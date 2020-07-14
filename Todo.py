ccd9baa28d3514278ac63927b4a44ccf7d28572e
Ellie Mae Galyean <elliemaegalyean@gmail.com>
2020-07-14 10:56:25 -0400

    inital commit

---
+++ Todo.py
@@ -0,0 +1,102 @@
+#!/bin/python3
+import yaml
+import datetime
+import pprint
+from terminaltables import AsciiTable as tabler
+import argparse
+
+pp = pprint.PrettyPrinter(indent=4)
+
+class Todo:
+    def __init__(self, file='/home/ellie/.config/eltodo/list.yaml'):
+        self.file = file
+        try:
+            with open(file) as f:
+                self.todo = yaml.safe_load(f.read())
+        except FileNotFoundError:
+            self.todo = []
+
+    def add(self, name: str, note=" ", urgency=0):
+        self.todo.append({'name': name, 'added': datetime.datetime.now(),
+                          'note': note, 'urgency' : urgency,
+                          'done' : None})
+        self.todo.sort(key=lambda x: x['added'])
+        self.todo.sort(key=lambda x: x['urgency'], reverse=True)
+        self.save()
+
+    def remove(self, index):
+        self.todo.pop(index)
+        self.save()
+
+    def done(self, index):
+        self.todo[index]['done'] = datetime.datetime.now()
+        self.save()
+
+    def dump(self):
+        pp.pprint(self.todo)
+
+    def __str__(self):
+        reset = '\033[0m' # Normal
+        italics = lambda x: f'\033[3m{x}{reset}'
+        strike = lambda x: f'\033[9m{x}'
+        urgency_colour_map = [
+            '\033[3m',   # Normal
+            '\033[33m',  # Yellow
+            '\033[31m',  # red
+            '\033[31;1;4m' # red and blinking
+        ]
+        table_data = [list(map(italics, ['#', 'Task', 'Note', 'Done']))]
+
+        for i in range(len(self.todo)):
+            urgency_colour = urgency_colour_map[self.todo[i]['urgency']]
+            table_data.append(
+                list(
+                    map(italics,
+                    [ i,
+                    f"{urgency_colour}{self.todo[i]['name']}",
+                    f"{urgency_colour}{self.todo[i]['note']}",
+                    "{}".format('\u2713' if self.todo[i]['done'] else '')
+                    ]
+                    )
+                )
+            )
+        return tabler(table_data, title='TODO').table
+
+    def __len__(self):
+        return len(self.todo)
+
+    def save(self):
+        with open(self.file, 'w') as f:
+            f.write(yaml.dump(self.todo))
+
+def main():
+    mainList = Todo()
+    p = pargs(len(mainList))
+    if "index" in vars(p):
+        mainList.remove(p.index)
+    elif "task" in vars(p):
+        mainList.add(p.task, note=p.note, urgency=p.urgency)
+    elif "doneindex" in vars(p):
+        mainList.done(p.doneindex)
+    print(mainList)
+
+def pargs(removalindexs):
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(help='add or remove task')
+    add_todo_parser = subparsers.add_parser("new", help="add new task")
+    add_todo_parser.add_argument("task", help='task name')
+    add_todo_parser.add_argument("-n", "--note", help='task note',
+                                 default="")
+    add_todo_parser.add_argument("-u", "--urgency", type=int,
+                                 choices=[0, 1, 2, 3], default=0)
+    removal_parser = subparsers.add_parser("remove", help="remove task")
+    removal_parser.add_argument("index", type=int, choices =
+                                range(removalindexs), help="index to remove")
+    done_parser = subparsers.add_parser("done", help="Set Task to Done")
+    done_parser.add_argument("doneindex", type = int,
+                             choices=range(removalindexs))
+    done_parser.add_argument
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Ellie's Dope af todo program
+
+### Installation
+```
+$ git clone https://github.com/Lifesgood123/eltodo.git
+$ cd eltodo && pip install -r requirements.txt
+```
+### Usage
+``````
+usage: ,todo [-h] {new,remove,done} ...
+
+positional arguments:
+  {new,remove,done}  add or remove task
+    new              add new task
+    remove           remove task
+    done             Set Task to Done
+
+optional arguments:
+  -h, --help         show this help message and exit
+``````
